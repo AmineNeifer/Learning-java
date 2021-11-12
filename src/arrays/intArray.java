c7adb5b80b84c84f539f9716285088c1ee82f8a0
Neifer <52834823+AmineNeifer@users.noreply.github.com>
2021-11-12 09:35:02 +0100

    corrected packages

--- src/arrays/intArray.java
+++ src/arrays/intArray.java
@@ -1,7 +1,6 @@
-package intArray;
+package arrays;
 
-public class Main {
-
+public class intArray {
     public static void main(String[] args) {
         int[] intArray = new int[7];
         intArray[0] = 0;
