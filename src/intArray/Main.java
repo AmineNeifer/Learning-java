4dfb44caefbcb4b585d1f637d32c9dd4702fc042
Neifer <52834823+AmineNeifer@users.noreply.github.com>
2021-11-12 09:27:56 +0100

    array of integers

---
+++ src/intArray/Main.java
@@ -0,0 +1,19 @@
+package intArray;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] intArray = new int[7];
+        intArray[0] = 0;
+        intArray[1] = 23;
+        intArray[2] = -123412;
+        intArray[3] = 1234123;
+        intArray[4] = 542;
+        intArray[5] = -12;
+        intArray[6] = 2;
+
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println(intArray[i]);
+        }
+    }
+}
