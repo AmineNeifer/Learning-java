7a87985139708a3f33cfb72140be234dcc505df8
Neifer <52834823+AmineNeifer@users.noreply.github.com>
2021-12-28 16:28:38 +0100

    did question 2

--- src/ExcepetionHandling/Exercice1/Main.java
+++ src/ExcepetionHandling/Exercice1/Main.java
@@ -1,13 +1,18 @@
 package ExcepetionHandling.Exercice1;
 
+import java.util.Scanner;
+
 import static java.lang.Integer.parseInt;
 
 public class Main {
     public static void main(String[] args) {
-        Integer sum = question1(args);
-        System.out.println(sum);
+        Integer q1 = question1(args);
+//        System.out.println(q1);
 
+        Integer q2 = question2();
+//        System.out.println(q2);
 
+        
     }
     private static Integer question1(String[] args) {
         int sum = 0;
@@ -17,5 +22,21 @@ public class Main {
             } catch (NumberFormatException e) { }
         }
         return sum;
+    }
+    private static Integer question2() {
+        int intInput = 0;
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter an Integer");
+
+            String inputUser = sc.nextLine();
+            try {
+                intInput = parseInt(inputUser);
+                break;
+            } catch (NumberFormatException e){
+                System.out.println("that is not an integer :'(");
+            }
+        }
+        return intInput;
     }
 }
